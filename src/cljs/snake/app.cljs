8d33e4fc36657e20b52368a02153b58678164854
Petrus Theron <github@petrus.co.za>
2015-08-13 11:04:57 +0200

    movement is now left or right. use rotation matrix for turning. can now die. shows high score round cells

--- src/cljs/snake/app.cljs
+++ src/cljs/snake/app.cljs
@@ -3,109 +3,119 @@
 
 (enable-console-print!)
 
-(def width 15)
-(def height 15)
+(def width 10)
+(def height 10)
+(def tick-interval 200)
 (def min-pills 3)
-(def paused? (atom false))
+(def init-size 3)
 
-(def init-state {:position [7 7]
-                 :history ()
-                 :velocity [-1 0]
-                 :size 3
-                 :pills #{[4 4] [5 8]}})
+(def init-state
+  {:paused?  false
+   :position [7 7]
+   :history  ()
+   :velocity [-1 0]
+   :size     init-size
+   :dead?    false
+   :pills    #{[4 4] [5 8]}})
 
 (defonce state (atom init-state))
+(def paused? (cursor state [:paused?]))
+(def dead? (cursor state [:dead?]))
 
-(def colors
+(def cell-colors
   {:head "gold"
    :worm "green"
    :pill "red"
    :wall "grey"})
 
-(defn cell-view [x y cell]
+(defn multiply-matrix
+  "Multiplies a vector by the columns of a matrix. For rotation."
+  [v cols]
+  (mapv #(apply + (map * % v)) cols))
+
+(defn cell-view [x y type]
   [:div
    {:style {:float         "left"
             :clear         (if (zero? x) "both")
-            :width         "32px"
-            :height        "32px"
-            :background    (get colors cell "#eee")
-            :border-right  "1px solid #222"
-            :border-bottom "1px solid #222"}}])
+            :width         "20px"
+            :height        "20px"
+            :background    (get cell-colors type "#eee")
+            :border-radius "0.5em"
+            :margin "1px"
+            :border-right  "1px solid #777"
+            :border-bottom "1px solid #777"}}])
+
+(def rotations
+  "Rotation matrices."
+  {:left [[0 1] [-1 0]]
+   :right [[0 -1] [1 0]]})
+
+(defn handle-keys! [event]
+  (let [key (.-keyCode event)]
+    (if (= key 32)                                          ; 32 is spacebar
+      (swap! paused? not)
+      (when-let [dir (case key
+                           37 :left
+                           39 :right
+                           nil)]
+        (.preventDefault event)
+        (swap! state assoc :direction dir))))
+  nil)
+
+(defn next-state [{:keys [position size velocity direction pills history] :as state}]
+  (let [snake         (set (take size history))
+        found-pill?   (pills position)
+        rotation      (get rotations direction [[1 0] [0 1]])
+        velocity      (multiply-matrix velocity rotation)
+        new-pos       (mapv mod (map + velocity position) [width height])
+        pills         (if found-pill? (disj pills position) pills)
+        missing-pills (- min-pills (count pills))
+        new-pills     (take missing-pills (repeatedly #(vector (rand-int width) (rand-int height))))]
+    (assoc state
+      :dead? (if (snake new-pos) true false)
+      :direction nil
+      :velocity velocity
+      :history (take 30 (conj history position))
+      :position new-pos
+      :pills (into pills new-pills)
+      :size (if found-pill? (inc size) size))))
+
+(defn tick! []
+  (if (and (not @dead?) (not @paused?))
+    (swap! state next-state)))
 
 (defn world-view [{:keys [pills size history position] :as state}]
   (let [worm (set (take size history))]
     [:div
      (for [y (range height)
            x (range width)]
-       (let [pos  [x y]
-             cell (cond
-                    (= position [x y]) :head
-                    (pills pos) :pill
-                    (worm pos) :worm)]
+       (let [pos       [x y]
+             cell-type (cond
+                         (= position pos) :head
+                         (worm pos)       :worm
+                         (pills pos)      :pill)]
          ^{:key [x y]}
-         [cell-view x y cell]))]))
-
-(def arrow-keys
-  {37 :left
-   38 :up
-   39 :right
-   40 :down
-   32 :space})
-
-(def directions
-  {:left [-1 0]
-   :up [0 -1]
-   :right [1 0]
-   :down [0 1]})
-
-(defn handle-keys! [e]
-  (let [key (get arrow-keys (.-keyCode e))]
-    (if (= key :space)
-      (swap! paused? not)
-      (if-let [new-vel (get directions key)]
-        (do
-          (swap! state assoc :velocity new-vel)
-          false)
-        true))))
-
-(defn setup-keys! []
-  (.addEventListener js/window "keydown" #(handle-keys! %)))
-
-(defn next-state [{:keys [pills size history position velocity] :as state}]
-  (let [pill? (pills position)
-        new-pos (mapv mod (map + velocity position) [width height])
-        pills (if pill? (disj pills position) pills)
-        new-pills (reduce (fn [acc v] (conj acc v)) pills (take (- min-pills (count pills)) (repeatedly (fn [] [(rand-int width) (rand-int height)]))))]
-    (assoc state
-      :history (take 30 (conj history position))
-      :position new-pos
-      :pills new-pills
-      :size (if pill? (inc size) size))))
-
-(defn tick! []
-  (if-not @paused?
-    (swap! state next-state)))
-
-(defn setup-interval! []
-  (js/setInterval #(tick!) 200))
+         [cell-view x y cell-type]))]))
 
 (defn parent-component []
   (let [size (cursor state [:size])]
     [:div
      [:h1 "Let's make snake!"]
-     [:button {:on-click #(reset! state init-state)} "Reset!"]
+     [:button {:on-click #(reset! state init-state)} "New Game"]
      [:button {:on-click #(tick!)} "Tick!"]
      [:button {:on-click #(swap! size inc)} "Grow!"]
      [:button {:on-click #(swap! paused? not)} "Pause"]
-     [:pre {:style {:font-size "1.6em" :white-space "normal"}}
-      (str (dissoc @state :history))]
-     [world-view @state]]))
+     (if (:dead? @state)
+       [:h2 "Game Over. Score: " (- (:size @state) init-size)]
+       [world-view @state])
+     [:pre {:style {:clear "left" :font-size "1.6em" :white-space "normal"}}
+      (str (dissoc @state :history))]]))
 
 (defn ^:export mount-component []
   (reagent/render-component [parent-component]
     (.getElementById js/document "container")))
 
-(defn ^:export init []
-  (setup-interval!)
-  (setup-keys!)
+(defn ^:export init []  
+  (.addEventListener js/window "keydown" #(handle-keys! %))
+  (js/setInterval #(tick!) tick-interval)
   (mount-component))
