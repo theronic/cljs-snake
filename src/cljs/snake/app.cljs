aed2b0882030646880881b25cef680d3a5bf019f
Petrus Theron <github@petrus.co.za>
2015-08-15 15:18:05 +0200

    simplify turning code by hard-coding 90 degree rotation matrices increase tick time to 250ms. Game was a bit hard. init-pos centre of grid press enter to play again

--- src/cljs/snake/app.cljs
+++ src/cljs/snake/app.cljs
@@ -5,13 +5,13 @@
 
 (def width 10)
 (def height 10)
-(def tick-interval 200)
+(def tick-interval 250)
 (def min-pills 3)
 (def init-size 3)
 
 (def init-state
   {:paused?  false
-   :position [7 7]
+   :position [(int (/ width 2)) (int (/ height 2))]
    :history  ()
    :velocity [-1 0]
    :size     init-size
@@ -28,54 +28,48 @@
    :pill "red"
    :wall "grey"})
 
-(defn multiply-matrix
-  "Multiplies a vector by the columns of a matrix. For rotation."
-  [v cols]
-  (mapv #(apply + (map * % v)) cols))
-
 (defn cell-view [x y type]
   [:div
    {:style {:float         "left"
-            :clear         (if (zero? x) "both")
-            :width         "20px"
-            :height        "20px"
+            :clear         (if (zero? x) "left")
+            :width         "24px"
+            :height        "24px"
             :background    (get cell-colors type "#eee")
             :border-radius "0.5em"
             :margin "1px"
             :border-right  "1px solid #777"
             :border-bottom "1px solid #777"}}])
 
-(def rotations
-  "Rotation matrices."
-  {:left [[0 1] [-1 0]]
-   :right [[0 -1] [1 0]]})
-
 (defn handle-keys! [event]
   (let [key (.-keyCode event)]
-    (if (= key 32)                                          ; 32 is spacebar
-      (swap! paused? not)
-      (when-let [dir (case key
-                           37 :left
-                           39 :right
-                           nil)]
-        (.preventDefault event)
-        (swap! state assoc :direction dir))))
+    (case key
+          32 (swap! paused? not)                            ; spacebar
+          13 (if @dead? (reset! state init-state))          ; enter
+          (when-let [dir (case key
+                               37 :left
+                               39 :right
+                               nil)]
+            (.preventDefault event)
+            (swap! state assoc :direction dir))))
   nil)
 
 (defn next-state [{:keys [position size velocity direction pills history] :as state}]
   (let [snake         (set (take size history))
-        found-pill?   (pills position)
-        rotation      (get rotations direction [[1 0] [0 1]])
-        velocity      (multiply-matrix velocity rotation)
-        new-pos       (mapv mod (map + velocity position) [width height])
+        found-pill?   (pills position)       
+        [dx dy]       velocity
+        new-vel       (case direction                       ; 90-degree rotation matrices
+                            :left [dy (- dx)]
+                            :right [(- dy) dx]
+                            [dx dy])
+        new-pos       (mapv mod (map + new-vel position) [width height])
         pills         (if found-pill? (disj pills position) pills)
         missing-pills (- min-pills (count pills))
         new-pills     (take missing-pills (repeatedly #(vector (rand-int width) (rand-int height))))]
     (assoc state
       :dead? (if (snake new-pos) true false)
       :direction nil
-      :velocity velocity
-      :history (take 30 (conj history position))
+      :velocity new-vel
+      :history (take 100 (conj history position))
       :position new-pos
       :pills (into pills new-pills)
       :size (if found-pill? (inc size) size))))
@@ -100,16 +94,23 @@
 (defn parent-component []
   (let [size (cursor state [:size])]
     [:div
-     [:h1 "Let's make snake!"]
-     [:button {:on-click #(reset! state init-state)} "New Game"]
-     [:button {:on-click #(tick!)} "Tick!"]
-     [:button {:on-click #(swap! size inc)} "Grow!"]
-     [:button {:on-click #(swap! paused? not)} "Pause"]
+     [:h1 {:style {:margin 0}} "Snake in ~100 lines of code"]
+     [:p
+      [:button {:on-click #(reset! state init-state)} "New Game"] " "
+      [:button {:on-click #(tick!)} "Tick!"] " "
+      [:button {:on-click #(swap! size inc)} "Grow!"] " "
+      [:button {:on-click #(swap! paused? not)} "Pause"]]
+     [:h2 "Score: " (- (:size @state) init-size)]
      (if (:dead? @state)
-       [:h2 "Game Over. Score: " (- (:size @state) init-size)]
+       [:div
+        [:h2 "Game Over"]
+        [:h3 "Press enter to try again :)"]]
        [world-view @state])
-     [:pre {:style {:clear "left" :font-size "1.6em" :white-space "normal"}}
-      (str (dissoc @state :history))]]))
+     [:pre {:style {:clear "left" :white-space "normal"}}
+      (str @state)]
+     [:p {:style {:clear "left"}} "Source Code:"
+      [:a {:href "https://github.com/pate/snake-cljs"}
+       "github.com/pate/snake-cljs"]]]))
 
 (defn ^:export mount-component []
   (reagent/render-component [parent-component]
